abf8476c6a09f6e3d6078772f5793b415bfa32ac
Riki <zccz14@outlook.com>
2016-08-28 11:13:03 +0800

    ADD: 快速排序

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,41 @@
+/*
+@title: 快速排序
+@description: 递归实现
+@arguments:
+    A: 待排序数组的首指针
+    p: 排序首地址
+    r: 排序尾地址
+@performance:
+    Time:
+        O(n log(n)) in expected
+        O(n^2) in worst
+    Space: O(n)
+        Extra:
+            O(log n) in expected
+            O(n) in worst
+@dependence:
+    std::swap in "algorithm", exactly in 'move.h'
+@note:
+    函数后 保证 A 升序，可重载 < 号，改变顺序
+    没有做随机化
+*/
+template <class T> void QuickSort(T *A, int p, int r) {
+  int i = p, j = r, x = A[(p + r) / 2];
+  while (i <= j) {
+    while (A[i] < x) {
+      i++;
+    }
+    while (x < A[j]) {
+      j--;
+    }
+    if (i <= j) {
+      std::swap(A[i++], A[j--]);
+    }
+  }
+  if (p < j) {
+    QuickSort(A, p, j);
+  }
+  if (i < r) {
+    QuickSort(A, i, r);
+  }
+}
