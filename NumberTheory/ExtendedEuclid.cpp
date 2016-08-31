a2831d7a6f5d403e4036a49fb55f9505bdccad35
Riki <zccz14@outlook.com>
2016-08-31 16:38:42 +0800

    ADD: 拓展欧几里得算法

---
+++ NumberTheory/ExtendedEuclid.cpp
@@ -0,0 +1,29 @@
+/*
+@title: 拓展欧几里得算法
+@description:
+    使用辗转相除法求GCD时，同时求出满足
+        a * x + b * y == gcd(a, b)
+    的(x, y)
+@arguments:
+    a, b: 两个不同时为零的整数
+    x, y: 两个整数容器 [alloc only]
+@performance:
+    Time: O(log(n))
+    Space: O(log(n))
+@return:
+    [int] gcd(a, b) in [1, max(a, b)]
+@note:
+    gcd(0, 0) 是未定义的，这个实现会返回 0
+    gcd(a, m) == 1 等价于 a 有模m乘法的逆元
+    调用 ExtendedEuclid(a, m, x, y) 后 x 等于该逆元
+    e.g. ExtendedEuclid(a, m, x, y) == 1? (x mod m): -1
+*/
+int ExtendedEuclid(int a, int b, int &x, int &y) {
+  if (b == 0) {
+    x = 1, y = 0;
+    return a;
+  }
+  int gcd = ExtendedEuclid(b, a % b, y, x);
+  y -= a / b * x;
+  return gcd;
+}
